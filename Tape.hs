0dc8e6410f9b7638ca0a9f8971379c8353454586
admin <rmansorokin@gmail.com>
2016-11-11 22:34:46 +0300

    Some fixes with module functions and types

--- Tape.hs
+++ Tape.hs
@@ -1,9 +1,16 @@
 module Tape
 (Tape
+,Dir
+,State
+,Shift
 ,getWord
 ,createTape)where
 
+data Dir = LeftSh | RightSh | Stay
 data Tape = Tape String String
+type State = Int
+type Shift = Dir
+
 createTape :: String -> Char -> Tape
 createTape word emptySymb = Tape bs (word ++ bs)
     where
@@ -11,3 +18,14 @@ createTape word emptySymb = Tape bs (word ++ bs)
 
 getWord :: Tape -> Char -> String
 getWord (Tape ls rs) s = (takeWhile (/=s) ls ) ++ (takeWhile (/=s) rs)
+
+shift :: Tape -> Dir -> Tape
+shift tape Stay = tape
+shift (Tape ls (r : rs)) RightSh = Tape (r : ls) rs
+shift (Tape (l : ls) rs) LeftSh = Tape ls (l : rs)
+
+getSymb :: Tape -> Char
+getSymb (Tape _ (r : _)) = r
+
+writeSymb :: Tape -> Char -> Tape
+writeSymb (Tape ls (_ : rs)) symb = Tape ls (symb : rs)
--- machine.hs
+++ machine.hs
@@ -2,21 +2,6 @@ import Data.Map as Map
 import Data.List as List
 import Tape
 
-data Dir = LeftSh | RightSh | Stay
-type State = Int
-type Shift = Dir
-
-shift :: Tape -> Dir -> Tape
-shift tape Stay = tape
-shift (Tape ls (r : rs)) RightSh = Tape (r : ls) rs
-shift (Tape (l : ls) rs) LeftSh = Tape ls (l : rs)
-
-getSymb :: Tape -> Char
-getSymb (Tape _ (r : _)) = r
-
-writeSymb :: Tape -> Char -> Tape
-writeSymb (Tape ls (_ : rs)) symb = Tape ls (symb : rs)
-
 turingMachine :: Map.Map (Char, State) (Char, State, Shift) -> (Tape, State) -> Tape
 turingMachine program (tape,state) = fst $ last $ List.unfoldr foldFunc (tape, state)
     where
