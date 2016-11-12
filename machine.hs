64198827823f2ba496c1f7f2aac6eb9e6ab684ca
admin <rmansorokin@gmail.com>
2016-11-12 23:18:31 +0300

    tests added

--- machine.hs
+++ machine.hs
@@ -2,13 +2,26 @@ import Data.Map as Map
 import Data.List as List
 import Tape
 
-turingMachine :: Map.Map (Char, State) (Char, State, Dir) -> (Tape, State) -> Tape
+turingMachine :: Map.Map (Char,State ) (Char, State, Dir) -> (Tape, State) -> Tape
 turingMachine program (tape,state) = fst $ last $ List.unfoldr foldFunc (tape, state)
     where
           foldFunc (tape, state) =
               let newTape = shift (writeSymb tape newSymb) dir
                   (newSymb, newState, dir) = case Map.lookup (getSymb tape, state) program of
                           Just (n,st,sh) -> (n, st, sh)
-                          Nothing -> (' ',0, Stay)
+                          Nothing -> (' ',0,Stay)
               in if state == 0 then Nothing
                  else Just ((tape, state), (newTape, newState))
+
+tests :: Bool
+tests = getWord (turingMachine inc (t, 1)) == "234"
+        && getWord (turingMachine inc (t2, 1)) == "432342"
+        && getWord (turingMachine bBeforeB (t3, 1)) == "bbbb"
+        && getWord (turingMachine bBeforeB (t4, 1)) == "abbabb"
+    where
+      inc =  Map.fromList [(('1', 1), ('2', 1, RightSh)), (('2', 1), ('3', 1, RightSh)), (('3', 1), ('4', 1, RightSh)), (('s', 1), ('s', 0, Stay))]
+      bBeforeB = Map.fromList [(('a', 1), ('a', 1, RightSh)), (('s', 1), ('s', 0, Stay)), (('b', 1), ('b', 2, LeftSh)), (('a', 2), ('b', 2, RightSh)), (('b', 2), ('b', 1, RightSh))]
+      t = Tape.createTape "123" 's'
+      t2 = Tape.createTape "321231" 's'
+      t3 = Tape.createTape "abab" 's'
+      t4 = Tape.createTape "aabaab" 's'
